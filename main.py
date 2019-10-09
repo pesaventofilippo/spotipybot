5df20487bebe32926ec004eac8eff6434c55de95
0x81ab81 <hexad81@aol.com>
2019-10-09 21:42:42 +0200

    Edited spotify module
    Used dictionaries instead of classes to collect static data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as selEc
 from selenium.webdriver.common.by import By as selBy
 from selenium.webdriver.support.ui import WebDriverWait
 import json, time, threading, random, sys
-from modules.spotify import Links as links, XPaths as xpaths
+from modules.spotify import links, xpaths
 
 
 def run(username, password, _url, _headless):
@@ -14,23 +14,23 @@ def run(username, password, _url, _headless):
     options.headless = _headless
     browser = Chrome(executable_path='drivers/chromedriver.exe', options=options)
     wait = WebDriverWait(browser, 10)
-    browser.get(_url) if _url else browser.get(links.default)
+    browser.get(_url) if _url else browser.get(links["default"])
 
     # Login with given credentials
-    login_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.login_btn)))
+    login_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["login_btn"])))
     login_btn.click()
-    user_form = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.user_form)))
+    user_form = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["user_form"])))
     user_form.send_keys(username)
-    pass_form = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.pass_form)))
+    pass_form = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["pass_form"])))
     pass_form.send_keys(password)
-    cookie_check = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.cookie_check)))
+    cookie_check = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["cookie_check"])))
     cookie_check.click()
-    submit_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.submit_btn)))
+    submit_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["submit_btn"])))
     submit_btn.click()
 
     # Configure shuffle and repeat settings
-    shuffle_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.shuffle_btn)))
-    repeat_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.repeat_btn)))
+    shuffle_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["shuffle_btn"])))
+    repeat_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["repeat_btn"])))
 
     # Activate shuffle if disabled
     if "control-button--active" not in shuffle_btn.get_attribute("class"):
@@ -50,14 +50,14 @@ def run(username, password, _url, _headless):
         repeat_btn.click()
 
     # Start playing
-    play_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.play_btn)))
+    play_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["play_btn"])))
     play_btn.click()
     while True:
         time.sleep(random.randint(55, 70))
-        song_name = wait.until(selEc.presence_of_element_located((selBy.XPATH, xpaths.song_name))).text
-        time_track = wait.until(selEc.presence_of_element_located((selBy.XPATH, xpaths.time_track))).text
+        song_name = wait.until(selEc.presence_of_element_located((selBy.XPATH, xpaths["song_name"]))).text
+        time_track = wait.until(selEc.presence_of_element_located((selBy.XPATH, xpaths["time_track"]))).text
         print(" * Played {0} for {1}".format(song_name, time_track))
-        skip_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths.skip_btn)))
+        skip_btn = wait.until(selEc.element_to_be_clickable((selBy.XPATH, xpaths["skip_btn"])))
         skip_btn.click()
 
 
